03a3d8dfb1516a9bc3d423b18a831ca321dec2d5
Adrian Adiaconitei <aadiaconitei@gmail.com>
2019-09-06 13:30:20 +0300

    Curs2 homework done

--- BookStore/src/app/app.module.ts
+++ BookStore/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { bootstrap } from "bootstrap";
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { NotebookListComponent } from './components/notebook-list/notebook-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    NotebookListComponent
   ],
   imports: [
     BrowserModule
