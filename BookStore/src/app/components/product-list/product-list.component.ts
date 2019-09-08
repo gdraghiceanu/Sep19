66c34be39102ded1849f8b94ff00de4f322a5254
unknown <gdraghiceanu@yahoo.com>
2019-09-08 23:45:00 +0300

    for curs 3

--- BookStore/src/app/components/product-list/product-list.component.ts
+++ BookStore/src/app/components/product-list/product-list.component.ts
@@ -7,11 +7,36 @@ import { books } from 'src/app/constants/books.seed';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  pageTitle = 'Book Store';
+  imageSize = 50;
+  showImage = true;
+
+  _filterList: string;
+  get filterList() {
+    return this._filterList;
+  }
+  set filterList(value: string) {
+    this._filterList = value;
+    this.filteredBooks = value ? this.produceFilterList(value) : this.books;
+  }
+
+  filteredBooks: Book[];
   books = books;
 
-  constructor() { }
+  constructor() { 
+    this.filteredBooks = this.books;
+  }
 
   ngOnInit() {
+  }
+
+  toggleImage() {
+    this.showImage = !this.showImage;
+  }
+
+  produceFilterList(keyValue: string ): Book[] {
+    keyValue = keyValue.toLocaleLowerCase();
+    return this.books.filter(book => book.title.toLocaleLowerCase().indexOf(keyValue) !== -1 );
   }
 
 }
