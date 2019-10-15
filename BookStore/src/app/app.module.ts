6df6aab0fc752790a3d6a2b49e479264af640dc3
unknown <hanchevicicodrut@gmail.com>
2019-10-15 14:04:00 +0300

    abc

--- BookStore/src/app/app.module.ts
+++ BookStore/src/app/app.module.ts
@@ -4,13 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { StarComponent } from './components/shared/star.component';
+import { StarComponent } from './components/shared/star.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    StarComponent
+    StarComponent, 
   ],
   imports: [
     BrowserModule,
