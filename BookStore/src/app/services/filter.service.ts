a659e5f480e244d9649a6e57dcbf7f6c386e5056
gdraghiceanu <george.draghiceanu@gmail.com>
2019-10-23 21:17:38 +0300

    Curs 7 implementation

---
+++ BookStore/src/app/services/filter.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterService {
+  filterValue$: Observable<string>;
+
+  private filterBehaviorSubject: BehaviorSubject<string> = new BehaviorSubject("");
+
+  constructor() {
+    this.filterValue$ = this.filterBehaviorSubject.asObservable();
+  }
+
+  changeFilter(value: string): void {
+    console.log('filter service change');
+
+    this.filterBehaviorSubject.next(value);
+  }
+}
